aeb18082b5e0959dcbb3a2b8479869b4efa1992f
isonguyom <isonguyom@gmail.com>
2023-01-15 21:49:52 +0100

    feat: create and implement onlyNums method, add card type rendering, and card type images

--- src/components/PaymentForm.vue
+++ src/components/PaymentForm.vue
@@ -1,6 +1,38 @@
 <script>
     export default {
-        props: ["total"]
+        props: ["total"],
+
+        data() {
+            return {
+                  card: "",
+                cardNum: "",
+            }
+        },
+
+        methods: {
+            onlyNums(e) {
+                e.target.value = e.target.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
+                this.checkCardType()
+            },
+
+            checkCardType() {
+                if (this.cardNum !== "") {
+                for (let i = 0; i < this.cardNum.length; i++) {
+                    if (this.cardNum[0] === "5") {
+                        this.card = "master"
+                    } else if (this.cardNum[0] === "7") {
+                        this.card = "visa"
+                    } else if (this.cardNum[0] === "2") {
+                        this.card = "verve"
+                    } else {
+                        this.card = ""
+                    } 
+                }
+            }else {
+                this.card = ""
+            }
+            }
+        },
     }
 
 </script>
@@ -9,9 +41,18 @@
     <div>
         <h2>Payment Details</h2>
         <div class="card-type-wrapper">
-            <div class="card-1"><h3>Master Card</h3></div>
-            <div class="card-2"><h3>Visa</h3></div>
-            <div class="card-3"><h3>Verve</h3></div>
+            <h1 class="mastercard" v-if="card === 'master'">
+                <img src="../assets/mastercard.svg" alt="Master Card">
+            </h1>
+            <h1 class="visa" v-else-if="card === 'visa'">
+                <img src="../assets/visa.svg" alt="Visa">
+            </h1>
+            <h1 class="verve" v-else-if="card === 'verve'">
+                <img src="../assets/verve.png" alt="Verve">
+            </h1>
+            <h1 class="no-card" v-else>
+                Invalid Card
+            </h1>
         </div>
         <form action="">
             <div class="form-control">
@@ -20,16 +61,16 @@
             </div>
             <div class="form-control">
                 <label for="cardNumber">Card Number</label>
-                <input type="number" name="card-number" id="cardNumber">
+                <input type="text" name="card-number" id="cardNumber" v-model="cardNum"  maxlength="19" @keyup="onlyNums">
             </div>
             <div class="form-control-wrapper">
                 <div class="form-control">
                     <label for="validity">Valid Until</label>
-                    <input type="date" name="validity" id="validity">
+                    <input type="text" name="validity" id="validity" placeholder="MM/YY" @input="onlyNums">
                 </div>
                 <div class="form-control">
                     <label for="cvc">CVC</label>
-                    <input type="number" name="cvc" id="cvc">
+                    <input type="password" name="cvc" id="cvc" maxlength="3" @input="onlyNums">
                 </div>
             </div>
             <button type="submit">Pay ₦{{ total }}</button>
@@ -44,29 +85,33 @@
     }
 .card-type-wrapper {
     display: flex;
-    justify-content: space-between;
+    justify-content: center;
+    align-items: center;
     margin: 10px 0 30px;
+    min-height: 100px;
 }
 
-.card-type-wrapper > div {
-    width: 30%;
-    height: 50px;
-    text-align: center;
-    color: #fff;
+.card-type-wrapper h1 {
+    text-transform: uppercase;
+    color: rgb(156, 154, 154);
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 
-.card-type-wrapper .card-1 {
-    background-color: orangered;
+.card-type-wrapper h1 img {
+    height: 90px;
+    display: block;
 }
 
-.card-type-wrapper .card-2 {
-    background-color: darkblue;
+.card-type-wrapper h1.no-card {
+    height: 90px;
+    background-color: rgb(212, 212, 212);
+    padding: 15px;
+    width: 100%;
+    font-weight: 700;
 }
 
-.card-type-wrapper .card-3 {
-    background-color: lightblue;
-    color: #343434;
-}
 
 form {
     width: 100%;
